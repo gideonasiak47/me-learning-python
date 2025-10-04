2f7992115281173b04324b483c38388fef2bfac1
Gideon Asiak <gideonasiak47@gmail.com>
2025-10-04 10:49:42 +0000

    first commit

---
+++ lesson 3/meaning.py
@@ -0,0 +1,13 @@
+meaning = 8
+print('')
+
+
+# if meaning > 10:
+#     print("Right on!")
+# else:
+#     print("Not today.")
+
+# Ternary operator
+print("Right on!") if meaning > 10 else print("Not today.")
+
+
---
+++ lesson 4/data_types.py
@@ -0,0 +1,9 @@
+# String data type
+
+# literal assignment
+first = "James"
+last = "Bond"
+
+print(type(first))
+print(type(first) == str)
+print(isinstance(first, str))
